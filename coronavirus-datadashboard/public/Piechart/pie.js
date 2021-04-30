7863ef3aa973e6ca78278980e12eb854fd3a410a
Nishi Anand <nishianand.05@gmail.com>
2021-04-30 04:52:29 +0000

    deploy

---
+++ coronavirus-datadashboard/public/Piechart/pie.js
@@ -0,0 +1,291 @@
+// ----------------------------------------- Default pie chart ----------------------------------------- 
+
+// Get date and attribute and create pie chart
+var dateInput = $("#dateInput").val();
+var attribute = "cases";
+$('#mainTitleDate').text(dateInput)
+piechart(dateInput, attribute);
+
+
+// ----------------------------------------- Pie chart with attr and date selected by user ----------------------------------------- 
+
+// Load button
+$("#refresh").click(function(){
+    // Get date and attribute and create pie chart
+    dateInput = $("#dateInput").val();
+    var radio = document.querySelectorAll(".radio");
+    for(var i=0;i<radio.length;i++){
+        if(radio[i].checked){
+            attribute = radio[i].value;
+        }
+    }
+    piechart(dateInput, attribute);
+});
+
+// ----------------------------------------- Pie chart function ----------------------------------------- 
+
+function piechart(dateInput, attribute){
+    
+    $('#mainTitleDate').text(dateInput)
+    
+    d3.queue()
+    .defer(d3.json, 'https://api.covid19tracking.narrativa.com/api/' + dateInput)
+    .awaitAll(function(error, data) {
+
+        console.log(error, data)
+
+        // Display alert message if error or no data
+        
+        if (error || !(data)){
+            alertMessage();
+        };
+
+        // Data arrays
+
+        var coroArr = formatAllData(data, dateInput);
+        var totData = totalData(data);
+
+        AddTotalData(totData);
+
+
+        //Tooltips
+
+        var tooltip = d3.select('body')
+                        .append('div')
+                            .classed('tooltip', true);
+
+        // SVG width and height
+
+        var width = 600;
+        var height = 600;
+        
+        var svg = d3.select('#pie')
+                    .attr('width', width)
+                    .attr('height', height);
+
+                    
+
+                            
+        // Pie chart scale
+
+        var colorScale = d3.scaleOrdinal()
+                        .domain(coroArr)
+                        .range(["#00876c",
+                                "#3d9c73",
+                                "#63b179",
+                                "#88c580",
+                                "#aed987",
+                                "#d6ec91",
+                                "#ffff9d",
+                                "#fee17e",
+                                "#fcc267",
+                                "#f7a258",
+                                "#ef8250",
+                                "#e4604e",
+                                "#d43d51"]);
+
+        
+        // Pie chart position
+        svg.append('g')
+        .attr('transform', `translate(${width/2}, ${height/2})`)
+        .classed('chart', true);
+
+
+        // Total data position
+        svg.append('g')
+        .attr('transform', `translate(${width/2}, ${height/2})`)
+        .classed('totData', true);
+
+
+        var tot = d3.select('.totData')
+    
+        tot
+            .append('text')
+                .text(`${attribute.charAt(0).toUpperCase() + attribute.slice(1)} Worldwide`)
+                .attr('text-anchor', 'middle')
+                .attr('dy', '-30px')
+                .style('font-weight', '500')
+                .style('fill','white')
+                .classed('totTitle', true)
+
+        tot
+        .append('text')
+            .text(`${totData[attribute].toLocaleString()}`)
+            .attr('text-anchor', 'middle')
+            .attr('dy', '20px')
+            .style('font-size', '2em')
+            .style('fill','white')
+            .classed('totAttr', true)
+                
+        var newAt;
+        attribute == 'cases'? newAt = 'newCases' : ( attribute == 'deaths' ? newAt = 'newDeaths' : newAt = 'newRecovered');
+        
+        tot
+        .append('text')
+            .text(`${(parseInt(totData[newAt])<0?"-":"+") + totData[newAt].toLocaleString()}`)
+            .attr('text-anchor', 'middle')
+            .attr('dy', '45px')
+            .style('font-size', '0.8em')
+            .style('fill', 'darkgrey')
+            .classed('totNewAttr', true)
+
+            
+
+
+        // Refresh button
+        d3.select('#refresh')
+          .on('click', function(){
+              makePie(coroArr, attribute);
+          });
+
+        makePie(coroArr, attribute);
+
+        function makePie(coroArr, attribute){
+
+            // Change title
+            d3.select('#pieTitle')
+            .text(`Piechart - ${attribute.charAt(0).toUpperCase() + attribute.slice(1)} (${dateInput})`);
+            
+            // Arcs 
+            var arcs = d3.pie()
+            .value(d => d[attribute])
+            .sort(function(a,b){
+                if(a.name < b.name) return -1;
+                else if (a.name > b.name) return 1;
+                else return a[attribute] - b[attribute];
+            })
+            (coroArr);
+        
+            // Paths
+            var path = d3.arc()
+                        .outerRadius(width/2 - 10)
+                        .innerRadius(120)
+        
+        
+        
+            // Update pattern
+            var update = d3.select('.chart')
+                            .selectAll('.arc')
+                            .data(arcs);
+        
+            update
+                .exit()
+                .remove();
+        
+            update
+                .enter()
+                .append('path')
+                    .classed('arc', true)
+
+                .merge(update)
+                    .attr('fill', d => colorScale(d.data.name))
+                    .attr('stroke', '#202124')
+                    .attr("stroke-width", "0")
+                    .attr('d', path)
+                    .on('mousemove', showTooltip)
+                    .on('touchstart', showTooltip)
+                    .on('mouseout', hideTooltip)
+                    .on('touchend', hideTooltip);
+        
+            
+            // Refresh btn 
+            d3.select('#refresh').on('click', function(){
+                
+                // loadpage();
+        
+                d3.select('.totTitle').remove()
+                d3.select('.totAttr').remove()
+                d3.select('.totNewAttr').remove()
+                d3.select('.totTitle')
+                        .text(`${attribute.charAt(0).toUpperCase() + attribute.slice(1)} Worldwide`)
+        
+                d3.select('.totAttr')
+                    .text(`${totData[attribute].toLocaleString()}`)
+                        
+                var newAt;
+                attribute == 'cases'? newAt = 'newCases' : ( attribute == 'deaths' ? newAt = 'newDeaths' : newAt = 'newRecovered');
+                // console.log(newAt)
+        
+                d3.select('.totNewAttr')
+                    .text(`${(parseInt(totData[newAt])<0?"-":"+") + totData[newAt].toLocaleString()}`)
+            
+            });
+        
+            // Tooltip functions
+        
+            function showTooltip(d) {
+
+                var arc = d3.select(this);
+                var isActive = arc.classed('highlight');
+                d3.selectAll('.arc').classed('highlight', false);
+                arc.classed('highlight', !isActive);
+
+                var Ttext = '<p>No data</p>';
+                if(d.data != undefined){
+                    Ttext =  `
+                    <p>Name: ${d.data.name}</p>
+                    <p>Date: ${d.data.date}</p>
+                    <p>Cases: ${d.data.cases.toLocaleString()}</p>
+                    <p>Recovered: ${d.data.recovered.toLocaleString()}</p>
+                    <p>Deaths: ${d.data.deaths.toLocaleString()}</p>
+                    `     
+                }
+                var tooltip = d3.select('.tooltip');
+                tooltip
+                    .style('opacity', 1)
+                    .style('left', ( d3.event.pageX - tooltip.node().offsetWidth/5) + 'px' )
+                    .style('top', ( d3.event.pageY- tooltip.node().offsetHeight/2 + 10) + 'px')
+                    .style('color', 'white')
+                    .html(Ttext)
+
+            }
+        
+            function hideTooltip(d) {
+                d3.select('.tooltip')
+                    .style('opacity', 0);
+            }
+        
+        }
+
+    });
+}
+    
+// ----------------------------------------- Format country data ----------------------------------------- 
+
+function formatAllData(data, dateInput) {
+
+    var countries = data[0]['dates'][dateInput]['countries']
+
+    var c = Object.keys(countries);
+    var ct = []
+
+    for(var i = 0; i< c.length; i++){
+        ct[i] = {
+            name: c[i],
+            date: countries[c[i]]['date'],
+            cases: countries[c[i]]['today_confirmed'],
+            recovered: countries[c[i]]['today_recovered'],
+            deaths: countries[c[i]]['today_deaths']
+        }
+    }
+    return ct;
+}
+
+// ----------------------------------------- Format total data ----------------------------------------- 
+
+function totalData(data){
+
+    var tt = data[0]['total']
+    var t = {
+        'date': tt['date'],
+        'cases': tt['today_confirmed'],
+        'newCases': tt['today_new_confirmed'],
+        'deaths': tt['today_deaths'],
+        'newDeaths': tt['today_new_deaths'],
+        'recovered': tt['today_recovered'],
+        'newRecovered': tt['today_new_recovered'],
+        'lastUpdated': data[0]['updated_at'] 
+    }
+    return t;
+}
+
---
+++ coronavirus-datadashboard/public/app.js
@@ -0,0 +1,72 @@
+// ----------------------------------------- Loading page ----------------------------------------- 
+
+function loadpage(){
+    $(".content").css("display", "none");
+    $(".loadingIcon").css("display", "block");
+    $(".loadingIcon").fadeOut(5000, function() {
+        $(".content").fadeIn(2000);        
+    });
+}
+
+$(window).on('load', function(){
+    $(document).ready(function() {
+        loadpage();
+    });   
+});
+
+
+// ----------------------------------------- Navbar ----------------------------------------- 
+
+$("#navBtn").click(function(){
+    // console.log('clicked');
+      $("nav").toggleClass("hide");
+      if($("#navBtn i").hasClass("fa-times")){
+        $("#navBtn i").removeClass("fa-times");
+        $("#navBtn i").addClass("fa-ellipsis-v navBtnColor");
+      } else {
+        $("#navBtn i").addClass("fa-times");
+        $("#navBtn i").removeClass("fa-ellipsis-v navBtnColor");
+      }
+});
+  
+
+$("#crossbtn").click(function(){
+    $(".alert").removeClass("show");
+    $(".alert").addClass("hide");
+    location.reload();
+})
+
+// ----------------------------------------- Date ----------------------------------------- 
+
+
+// Find yesterday date and set as default
+var date = new Date();
+var yesterday = date - 1000 * 60 * 60 * 24 * 1;
+yesterday = new Date(yesterday).toJSON().slice(0,10);
+$("#dateInput").val(yesterday);
+
+
+// ----------------------------------------- Alert message function ----------------------------------------- 
+
+function alertMessage(){
+    $(".alert").removeClass("hide");
+    $(".alert").addClass("show");
+  }
+  
+  
+  // ------------------------------------------- Add total data function --------------------------------------
+  
+  function AddTotalData(totData) {
+  
+    $("#tCasesValue").text(`${totData['cases'].toLocaleString()}`);
+    $("#newtCasesValue").text(`${(parseInt(totData['newCases'])<0?"-":"+") + totData['newCases'].toLocaleString()}`);
+  
+    $("#tDeathsValue").text(`${totData['deaths'].toLocaleString()}`);
+    $("#newtDeathsValue").text(`${(parseInt(totData['newDeaths'])<0?"-":"+") + totData['newDeaths'].toLocaleString()}`);
+  
+    $("#tRecoveredValue").text(`${totData['recovered'].toLocaleString()}`);
+    $("#newtRecoveredValue").text(`${(parseInt(totData['newRecovered'])<0?"-":"+") + totData['newRecovered'].toLocaleString()}`);
+    $('#lastUpdated p').text(`Last updated on ${totData['lastUpdated']}`);
+  
+  
+  }
