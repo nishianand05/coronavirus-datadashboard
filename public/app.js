04b0a85a65b10cd4611ee590b8f02fb832206b68
Nishi Anand <nishianand.05@gmail.com>
2021-07-15 13:32:37 +0000

    PWA

--- public/app.js
+++ public/app.js
@@ -1,3 +1,7 @@
+if('serviceWorker' in navigator){
+	navigator.serviceWorker.register('/sw.js');
+}
+
 // ----------------------------------------- Loading page ----------------------------------------- 
 
 function loadpage(){
